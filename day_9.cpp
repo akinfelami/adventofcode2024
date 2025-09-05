f3982e87c32453dc282b46b219f31c5dd0172065
Akinfolami Akin-Alamu <aoa9@cornell.edu>
2025-09-05 16:32:15 -0700

    day 9 done

--- day_9.cpp
+++ day_9.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -6,83 +7,149 @@
 using namespace std;
 
 int p1() {
-    ifstream file("input.txt");
-    if (file.is_open()) {
-        string line;
-        vector<string> numbers;
-        getline(file, line);
-        int id = 0;
-        for (size_t i = 0; i < line.size(); i++) {
-            int curr = line[i] - '0';
-            if ((i & 1) == 1) {
-                // odd so free space
-                while (curr > 0) {
-                    numbers.push_back(".");
-                    curr--;
-                }
-            } else {
-                while (curr > 0) {
-                    numbers.push_back(to_string(id));
-                    curr--;
-                }
-                id += 1;
-            }
+  ifstream file("input.txt");
+  if (file.is_open()) {
+    string line;
+    vector<string> numbers;
+    getline(file, line);
+    int id = 0;
+    for (size_t i = 0; i < line.size(); i++) {
+      int curr = line[i] - '0';
+      if ((i & 1) == 1) {
+        // odd so free space
+        while (curr > 0) {
+          numbers.push_back(".");
+          curr--;
         }
-        size_t dot_pos = 0;
-        size_t left_number_pos = numbers.size() - 1;
-        while (dot_pos < left_number_pos) {
-            if (numbers[dot_pos] == ".") {
-                if (numbers[left_number_pos] != ".") {
-                    numbers[dot_pos] = numbers[left_number_pos];
-                    numbers[left_number_pos] = ".";
-                    left_number_pos--;
-                    dot_pos++;
-                } else {
-                    left_number_pos--;
-                }
-            } else {
-                dot_pos++;
-            }
+      } else {
+        while (curr > 0) {
+          numbers.push_back(to_string(id));
+          curr--;
         }
-        unsigned long long total = 0ULL;
-        for (size_t i = 0; i < numbers.size(); i++) {
-            string num = numbers[i];
-            if (num != ".") {
-                unsigned long long number = (unsigned long long)(stoi(num));
-                total += (unsigned long long)(i)*number;
-            } else {
-                break;
-            }
-        }
-        cout << total;
-        cout << endl;
-    } else {
-        cout << "Unable to open file" << endl;
+        id += 1;
+      }
     }
+    size_t dot_pos = 0;
+    size_t left_number_pos = numbers.size() - 1;
+    while (dot_pos < left_number_pos) {
+      if (numbers[dot_pos] == ".") {
+        if (numbers[left_number_pos] != ".") {
+          numbers[dot_pos] = numbers[left_number_pos];
+          numbers[left_number_pos] = ".";
+          left_number_pos--;
+          dot_pos++;
+        } else {
+          left_number_pos--;
+        }
+      } else {
+        dot_pos++;
+      }
+    }
+    unsigned long long total = 0ULL;
+    for (size_t i = 0; i < numbers.size(); i++) {
+      string num = numbers[i];
+      if (num != ".") {
+        unsigned long long number = (unsigned long long)(stoi(num));
+        total += (unsigned long long)(i)*number;
+      } else {
+        break;
+      }
+    }
+    cout << total;
+    cout << endl;
+  } else {
+    cout << "Unable to open file" << endl;
+  }
 
-    return 0;
+  return 0;
 }
 
-int p2() {
-    ifstream file("input.txt");
-    if (file.is_open()) {
-        string line;
-        while (getline(file, line)) {
-            istringstream iss(line);
-            char c;
-            vector<char> freqs;
-            while (iss.get(c)) {
-                freqs.push_back(c);
-            }
+void attempt_to_move(int dot_pos, int right_pos, vector<string> &nums,
+                     int count) {
+  int n = nums.size();
+  while (dot_pos < (right_pos - count + 1)) {
+    if (nums[dot_pos] == ".") {
+      int k = dot_pos;
+      int number_of_free_space = 0;
+      while (k < n && nums[k] == ".") {
+        k++;
+        number_of_free_space++;
+      }
+      if (number_of_free_space >= count) {
+        // move it
+        for (int moved = 0; moved < count; ++moved) {
+          nums[dot_pos + moved] = nums[right_pos - moved];
+          nums[right_pos - moved] = ".";
         }
+        dot_pos += count;
+        right_pos -= count;
+        return;
+      } else {
+        dot_pos = k;
+      }
     } else {
-        cout << "Unable to open file" << endl;
+      dot_pos++;
     }
+  }
+}
+int p2() {
+  ifstream file("input.txt");
+  if (file.is_open()) {
+    string line;
+    vector<string> numbers;
+    getline(file, line);
+    int id = 0;
+    for (size_t i = 0; i < line.size(); i++) {
+      int curr = line[i] - '0';
+      if ((i & 1) == 1) {
+        // odd so free space
+        while (curr > 0) {
+          numbers.push_back(".");
+          curr--;
+        }
+      } else {
+        while (curr > 0) {
+          numbers.push_back(to_string(id));
+          curr--;
+        }
+        id += 1;
+      }
+    }
+    int left_number_pos = numbers.size() - 1;
+    while (left_number_pos >= 0) {
+      if (numbers[left_number_pos] != ".") {
+        int l = left_number_pos;
+        string left_number = numbers[left_number_pos];
+        int count = 0;
+        while (numbers[l] == left_number) {
+          l--;
+          count += 1;
+        }
+        // find a spot for it.
+        attempt_to_move(0, left_number_pos, numbers, count);
+        left_number_pos -= count;
+      } else {
+        left_number_pos--;
+      }
+    }
+    unsigned long long total = 0ULL;
+    for (size_t i = 0; i < numbers.size(); i++) {
+      string num = numbers[i];
+      if (num != ".") {
+        unsigned long long number = (unsigned long long)(stoi(num));
+        total += (unsigned long long)(i)*number;
+      }
+    }
+    cout << total;
+    cout << endl;
+  } else {
+    cout << "Unable to open file" << endl;
+  }
 
-    return 0;
+  return 0;
 }
 
 int main() {
-    p1();
-    return 0;
+  p2();
+  return 0;
 }
