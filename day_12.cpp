84c4ef79d50f4714baf3e20977429537af9d8ff0
Akinfolami Akin-Alamu <aoa9@cornell.edu>
2025-09-06 20:19:38 -0700

    day 12 part 1

---
+++ day_12.cpp
@@ -0,0 +1,108 @@
+#include <cstddef>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+using namespace std;
+
+struct node {
+  size_t row;
+  size_t col;
+  char val;
+};
+
+vector<vector<int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+void compute_area(vector<vector<node>> &grid, int row, int col, int &area,
+                  char val) {
+  if (row < 0 || row >= grid.size() || col < 0 || col >= grid[0].size()) {
+    area += 1;
+    return;
+  }
+  if (grid[row][col].val != val) {
+    area += 1;
+  }
+}
+
+void dfs(vector<vector<node>> &grid, int row, int col,
+         vector<vector<bool>> &visited, char val, int &perimeter, int &area) {
+  if (row < 0 || row >= grid.size() || col < 0 || col >= grid[0].size() ||
+      visited[row][col]) {
+    return;
+  }
+  if (grid[row][col].val != val) {
+    return;
+  }
+  perimeter += 1;
+  visited[row][col] = true;
+
+  // add area
+  for (auto &dir : directions) {
+    compute_area(grid, row + dir[0], col + dir[1], area, val);
+  }
+
+  for (auto &dir : directions) {
+    int newRow = row + dir[0];
+    int newCol = col + dir[1];
+    dfs(grid, newRow, newCol, visited, val, perimeter, area);
+  }
+}
+
+int p1() {
+  ifstream file("input.txt");
+  vector<vector<node>> graph;
+  unordered_set<char> plots;
+  if (file.is_open()) {
+    string line;
+    size_t row = 0;
+    while (getline(file, line)) {
+      istringstream iss(line);
+      char c;
+      vector<node> nodes;
+      size_t col = 0;
+      while (iss.get(c)) {
+        node n = node{row, col, c};
+        nodes.push_back(n);
+        col++;
+      }
+      row++;
+      graph.push_back(nodes);
+    }
+    unordered_map<char, pair<int, int>> props;
+    size_t rows = graph.size();
+    size_t cols = graph[0].size();
+    vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+    int total = 0;
+    for (size_t i = 0; i < graph.size(); i++) {
+      for (size_t j = 0; j < graph.size(); j++) {
+        node this_node = graph[i][j];
+        if (!visited[i][j]) {
+          int perimeter = 0;
+          int area = 0;
+          dfs(graph, i, j, visited, this_node.val, perimeter, area);
+          total += (perimeter * area);
+        }
+      }
+    }
+    // for (size_t i = 0; i < graph.size(); i++) {
+    //   for (size_t j = 0; j < graph.size(); j++) {
+    //     cout << graph[i][j].val;
+    //   }
+    //   cout << endl;
+    // }
+    cout << total << endl;
+  } else {
+    cout << "Unable to open file" << endl;
+  }
+
+  return 0;
+}
+
+int main() {
+  p1();
+  return 0;
+}
